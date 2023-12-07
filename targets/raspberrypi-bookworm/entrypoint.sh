18aa3593d5c042a4fc823f180ebd0dfbd535c989
Vincent Tavernier <v.tavernier@pm.me>
2023-12-07 22:15:29 +0100

    fix: fix build issues

--- targets/raspberrypi-bookworm/entrypoint.sh
+++ targets/raspberrypi-bookworm/entrypoint.sh
@@ -14,8 +14,8 @@ fi
 
 if [ "$ENABLE_PYO3" = "1" ]; then
   export PYO3_CROSS=1
-  export PYO3_CROSS_PYTHON_VERSION=3.9
-  export PYO3_CROSS_LIB_DIR=/opt/cross/lib/arm-linux-gnueabihf
+  export PYO3_CROSS_PYTHON_VERSION=3.11
+  export PYO3_CROSS_LIB_DIR=/opt/cross/lib/arm-linux-gnueabihf/python3.11
   export RUSTFLAGS="-C link-arg=-L/opt/cross/lib/arm-linux-gnueabihf -C link-arg=-lexpat -C link-arg=-lz"
 fi
 
